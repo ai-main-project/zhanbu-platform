4ca6d09574317e718e16b6b434d56a244f260b5b
wangyangyang <wangyangyang-015@cpic.com.cn>
2025-07-31 15:36:07 +0800

    feat: 样式调整

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -42,13 +42,6 @@ const config: Config = {
     },
   },
   plugins: [],
-  // Add PostCSS configuration directly here
-  postcss: {
-    plugins: {
-      '@tailwindcss/postcss': {},
-      autoprefixer: {},
-    },
-  },
 };
 
 export default config;
